bee90fce95712dd2625d9efc47872c20cce0638c
Yevhen <onufrii.socnet@gmail.com>
2023-10-23 13:29:10 +0300

    change: pressed the inputes to the left side

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -6,7 +6,7 @@ import ChangePassword from './parts/ChangePassword'
 import ModalWind from './parts/ModalWind'
 import TabPanel from './parts/TabPanel'
 
-import { ContentBox, CustomDivider, SettingsContainer, TabsContainer } from './styles'
+import { ContentBox, CustomDivider, MainContainer, SettingsHeaderContainer } from './styles'
 
 const Settings: FC = () => {
   const [value, setValue] = useState(0)
@@ -22,33 +22,35 @@ const Settings: FC = () => {
 
   return (
     <Stack sx={{ width: '100%' }}>
-      <SettingsContainer>
+      <SettingsHeaderContainer>
         <Typography variant="h3" component="h1">
           Налаштування
         </Typography>
-      </SettingsContainer>
+      </SettingsHeaderContainer>
 
       <Divider flexItem />
 
-      <TabsContainer>
+      <MainContainer>
         <Tabs value={value} onChange={handleChange}>
           <Tab disableRipple label="Змінити пароль" />
           <Tab disableRipple label="Змінити логін" />
         </Tabs>
-      </TabsContainer>
+      </MainContainer>
 
       <CustomDivider />
 
-      <ContentBox>
-        <TabPanel index={0} value={value}>
-          <ChangePassword openModal={openModal} />
-        </TabPanel>
-        <TabPanel index={1} value={value}>
-          <ChangeLogin openModal={openModal} />
-        </TabPanel>
+      <MainContainer>
+        <ContentBox>
+          <TabPanel index={0} value={value}>
+            <ChangePassword openModal={openModal} />
+          </TabPanel>
+          <TabPanel index={1} value={value}>
+            <ChangeLogin openModal={openModal} />
+          </TabPanel>
 
-        <ModalWind {...{ closeModal, open }} />
-      </ContentBox>
+          <ModalWind {...{ closeModal, open }} />
+        </ContentBox>
+      </MainContainer>
     </Stack>
   )
 }
