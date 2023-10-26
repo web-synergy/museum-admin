10f0cc6e29f27522e525e77f97daff1dd678ebc5
Anna Prutnik <prutnik.anna@gmail.com>
2023-10-26 16:49:12 +0300

    fix: add proxy in viteconfig

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,11 @@
 import { IEventValues, IContactInfo, IEvent } from '@/types/events';
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_SERVER_URL;
+// const BASE_URL = import.meta.env.VITE_SERVER_URL;
 // console.log(BASE_URL);
 
 export const instance = axios.create({
-  baseURL: BASE_URL,
+  // baseURL: BASE_URL,
 });
 
 interface LoginResponse {
@@ -15,22 +15,22 @@ interface LoginResponse {
 
 export const login = (username: string, password: string) => {
   return instance.post<LoginResponse>(
-    '/admin/login',
+    '/api/admin/login',
     {},
     { auth: { username, password } }
   );
 };
 
 export const logout = () => {
-  return instance.post('/admin/logout');
+  return instance.post('/api/admin/logout');
 };
 
 export const getContactInfo = () => {
-  return instance.get('/museum_data');
+  return instance.get('/api/museum_data');
 };
 
 export const updateContactInfo = (data: IContactInfo) => {
-  return instance.put('/admin/museum_data', data);
+  return instance.put('/api/admin/museum_data', data);
 };
 
 interface AddImageResponse {
@@ -41,19 +41,19 @@ export const addNewImage = (file: File | Blob) => {
   const headers = { 'Content-Type': 'multipart/form-data' };
   const formData = new FormData();
   formData.append('file', file);
-  return instance.post<AddImageResponse>('/admin/images', formData, {
+  return instance.post<AddImageResponse>('/api/admin/images', formData, {
     headers,
   });
 };
 
 export const getImage = (imageId: string) => {
-  return instance.get(`/images?filename=${imageId}&type=ORIGINAL`, {
+  return instance.get(`/api/images?filename=${imageId}&type=ORIGINAL`, {
     responseType: 'blob',
   });
 };
 
 export const addEvent = (data: IEventValues) => {
-  return instance.post('/admin/events', data);
+  return instance.post('/api/admin/events', data);
 };
 
 interface GetEventsResponse {
@@ -64,12 +64,14 @@ interface GetEventsResponse {
 }
 
 export const getEvents = (page = 0, size = 5) => {
-  return instance.get<GetEventsResponse>(`/events?size=${size}&page=${page}`);
+  return instance.get<GetEventsResponse>(
+    `/api/events?size=${size}&page=${page}`
+  );
 };
 
 export const getEventById = async (id: string) => {
   try {
-    const { data } = await instance.get<IEvent>(`/events/${id}`);
+    const { data } = await instance.get<IEvent>(`/api/events/${id}`);
     return data;
   } catch (e) {
     return null;
@@ -77,9 +79,9 @@ export const getEventById = async (id: string) => {
 };
 
 export const editEvent = async (data: IEventValues, id: string) => {
-  return instance.put(`/admin/events/${id}`, data);
+  return instance.put(`/api/admin/events/${id}`, data);
 };
 
 export const deleteEvent = async (id: string) => {
-  return instance.delete(`/admin/events/${id}`);
+  return instance.delete(`/api/admin/events/${id}`);
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,26 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log(env.VITE_SERVER_URL);
   const config = {
     plugins: [react()],
     base: '/',
     server: {
       port: 5174,
       host: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          ws: true,
+        },
+      },
     },
     resolve: {
       alias: {
