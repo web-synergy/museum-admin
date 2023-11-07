d06faaa294609115dfba6c4463ce8eede9e4d814
Anna Prutnik <prutnik.anna@gmail.com>
2023-11-07 09:22:16 +0200

    fix: chache to rpocess.env

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,11 @@
 import { rewrite } from '@vercel/edge';
-import { loadEnv } from 'vite';
 
 export default function proxySettings(request: Request) {
   const url = new URL(request.url);
-  const mode = request.mode;
-  const env = loadEnv(mode, process.cwd());
+  const serverUrl = process.env.VITE_SERVER_URL;
 
   if (url.pathname.startsWith('/api')) {
-    return rewrite(new URL(`${env.VITE_SERVER_URL}`, request.url));
+    return rewrite(new URL(`${serverUrl}`, request.url));
   }
 
   if (url.pathname.startsWith('/')) {
