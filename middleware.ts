cbcc33e55f7c9f0dfacaddb02b869653f557961d
Anna Prutnik <prutnik.anna@gmail.com>
2023-11-07 10:22:02 +0200

    fix: change env variable

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { rewrite } from '@vercel/edge';
 
 export default function proxySettings(request: Request) {
   const url = new URL(request.url);
-  const serverUrl = process.env.VITE_SERVER_URL;
+  const serverUrl = process.env.VITE_SERVER_URL_API;
 
   if (url.pathname.startsWith('/api')) {
     return rewrite(new URL(`${serverUrl}`, request.url));
