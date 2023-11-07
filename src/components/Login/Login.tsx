d2b42d38416c433dbc68d7a5a90cc629bd875160
Anna Prutnik <prutnik.anna@gmail.com>
2023-11-07 11:06:35 +0200

    fix: add get request to check server request

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,36 +1,41 @@
-import useAuth from '@/hooks/useAuth'
-import { Container, Typography } from '@mui/material'
-import { FC, FormEventHandler, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Section from '../Common/Section'
-import LoginForm from './parts/LoginForm'
-import { ContentBox } from './styles'
+import useAuth from '@/hooks/useAuth';
+import { Container, Typography } from '@mui/material';
+import { FC, FormEventHandler, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Section from '../Common/Section';
+import LoginForm from './parts/LoginForm';
+import { ContentBox } from './styles';
+import { getContactInfo } from '@/api';
 
 export interface AuthData {
-  login: string
-  password: string
+  login: string;
+  password: string;
 }
 
 const Login: FC = () => {
-  const { signIn } = useAuth()
-  const navigate = useNavigate()
+  const { signIn } = useAuth();
+  const navigate = useNavigate();
 
   const [authData, setAuthData] = useState<AuthData>({
     login: '',
     password: '',
-  })
-  const [loginError, setLoginError] = useState(false)
+  });
+  const [loginError, setLoginError] = useState(false);
 
-  const onSubmit: FormEventHandler = async e => {
-    e.preventDefault()
+  const onSubmit: FormEventHandler = async (e) => {
+    e.preventDefault();
 
-    if (!authData.login || !authData.password) return setLoginError(true)
+    if (!authData.login || !authData.password) return setLoginError(true);
 
-    const login = await signIn(authData.login, authData.password)
+    const login = await signIn(authData.login, authData.password);
 
-    if (login) navigate('/', { replace: true })
-    else setLoginError(true)
-  }
+    if (login) navigate('/', { replace: true });
+    else setLoginError(true);
+  };
+
+  useEffect(() => {
+    getContactInfo().then((res) => console.log(res));
+  }, []);
 
   return (
     <Section variant="light">
@@ -39,11 +44,13 @@ const Login: FC = () => {
           <Typography variant="h3" textAlign={'center'}>
             Вхід
           </Typography>
-          <LoginForm {...{ authData, setAuthData, onSubmit, loginError, setLoginError }} />
+          <LoginForm
+            {...{ authData, setAuthData, onSubmit, loginError, setLoginError }}
+          />
         </ContentBox>
       </Container>
     </Section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
