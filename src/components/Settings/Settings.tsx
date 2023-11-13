af9ab4196b0cc08300e934cf37e62fa5eb100a87
Yevhen <onufrii.socnet@gmail.com>
2023-11-13 13:37:27 +0200

    fix: change password tab on settings page

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,14 +1,28 @@
 import { Divider, Stack, Tab, Tabs, Typography } from '@mui/material'
 import { FC, SyntheticEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import ChangeLogin from './parts/ChangeLogin'
 import ChangePassword from './parts/ChangePassword'
 import TabPanel from './parts/TabPanel'
 
+import InfoModal from '../EventForm/parts/InfoModal'
 import { ContentBox, CustomDivider, MainContainer, SettingsHeaderContainer } from './styles'
+
+import useAuth from '@/hooks/useAuth'
 
 const Settings: FC = () => {
   const [value, setValue] = useState(0)
+  const [open, setOpen] = useState(false)
+
+  const { signOut } = useAuth()
+  const navigate = useNavigate()
+
+  const onClose = () => {
+    setOpen(false)
+    signOut()
+    navigate('/login', { replace: true })
+  }
 
   const handleChange = (event: SyntheticEvent, newValue: number) => {
     event.stopPropagation()
@@ -37,13 +51,15 @@ const Settings: FC = () => {
       <MainContainer>
         <ContentBox>
           <TabPanel index={0} value={value}>
-            <ChangePassword />
+            <ChangePassword {...{ setOpen }} />
           </TabPanel>
           <TabPanel index={1} value={value}>
             <ChangeLogin />
           </TabPanel>
         </ContentBox>
       </MainContainer>
+
+      <InfoModal {...{ text: 'Зміни збережено.', open, onClose }} />
     </Stack>
   )
 }
